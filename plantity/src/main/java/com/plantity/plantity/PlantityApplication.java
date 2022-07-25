401945368151c4b309dbb7bc5ad3d6797712d2a9
abi-hong <68041042+abi-hong@users.noreply.github.com>
2022-07-25 17:38:50 +0900

    initial : initial commit

---
+++ plantity/src/main/java/com/plantity/plantity/PlantityApplication.java
@@ -0,0 +1,13 @@
+package com.plantity.plantity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantityApplication.class, args);
+	}
+
+}
---
+++ plantity/src/test/java/com/plantity/plantity/PlantityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantity.plantity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
