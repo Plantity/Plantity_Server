0566d34fdd6704dd2d501ed91357d2fbc235e854
abi-hong <68041042+abi-hong@users.noreply.github.com>
2022-08-02 00:48:12 +0900

    [FEAT] #1 - design PlantFollowing entity
    - Need refactor code
    - DB를 다시보니, 연관관계를 고려한다면 Plant entity를 먼저 설계한 후, PlantFollowing을 만들어야했다.

--- plantity/src/main/java/com/plantity/domain/PlantFollowing.java
+++ plantity/src/main/java/com/plantity/domain/PlantFollowing.java
@@ -1,5 +1,28 @@
 package com.plantity.domain;
 
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+@Getter
 public class PlantFollowing {
-    
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private int following_idx; //찜한식물번호
+    private String status; //식물상태
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
